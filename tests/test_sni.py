99a9d6ada7951d489cd5293cda7e4ae1b8febc2d
Adar Nimrod <nimrod@shore.co.il>
2017-02-01 10:52:52 +0200

    - Added TLS 1.2 test.

--- tests/test_sni.py
+++ tests/test_sni.py
@@ -4,8 +4,15 @@ from testinfra.utils.ansible_runner import AnsibleRunner
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-@pytest.mark.parametrize('error', ['InsecureRequestWarning',
-                                   'InsecurePlatformWarning',
-                                   'SNIMissingWarning'])
+@pytest.mark.parametrize('error', [
+    'InsecureRequestWarning', 'InsecurePlatformWarning', 'SNIMissingWarning'
+])
 def test_sni(Command, error):
-    assert error not in Command('pip install nonexistant-package').stderr
+    assert error not in Command('pip2 install nonexistant-package').stderr
+
+
+def test_tls12(Command):
+    pycommand = '''import requests;
+print(requests.get('https://www.howsmyssl.com/a/check',
+verify=False).json()['tls_version'])'''
+    assert 'TLS 1.2' == Command('python2 -c \"%s\"' % pycommand).stdout.strip()
